4aef40678c4ccf1fcee705d764fe5711eacb0665
Muhammad Shah <muhammadshah.14@sunymaritime.edu>
2023-02-23 01:41:15 -0500

    springboot 3.0

--- src/main/java/com/example/demo/SecurityConfiguration.java
+++ src/main/java/com/example/demo/SecurityConfiguration.java
@@ -3,12 +3,13 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -37,8 +38,10 @@ public class SecurityConfiguration {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/", "/h2-console/**").permitAll()//.hasAnyRole("USER", "ADMIN")
                         .requestMatchers("/admin").hasRole("ADMIN")//.hasAuthority("ROLE_ADMIN")
+
                         .anyRequest().authenticated()
                 )
+//                .userDetailsService(userDetailsService)
                 .formLogin(form -> form
                         .loginPage("/login")//.failureUrl("/login?error=true")
                         .permitAll())
@@ -56,21 +59,12 @@ public class SecurityConfiguration {
     }
 
     @Bean
-    public AuthenticationProvider userDetailsService(BCryptPasswordEncoder passwordEncoder) {
+    public AuthenticationManager userDetailsService(HttpSecurity http) throws Exception {
         userDetailsService = new SSUserDetailsService(appUserRepository);
-
-        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setUserDetailsService(userDetailsService);
-        provider.setPasswordEncoder(passwordEncoder);
-        return provider;
-    }
-
-/*    @Bean
-    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
         return http.getSharedObject(AuthenticationManagerBuilder.class)
                 .userDetailsService(userDetailsService)
                 .passwordEncoder(passwordEncoder())
                 .and()
                 .build();
-    }*/
+    }
 }
